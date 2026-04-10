76c5f42549a7b4d41f523b3b70ae160ee87a238a
hgim96715-lgtm <hgim96715@gmail.com>
2026-04-11 06:41:01 +0900

    Time: 573 ms (96.54%), Space: 0B (100%) - LeetHub

--- 0184-department-highest-salary/0184-department-highest-salary.sql
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -1,7 +1,7 @@
 # Write your MySQL query statement below
 WITH join_to AS(
     SELECT d.name AS Department,e.name AS Employee,e.salary AS Salary,
-    RANK()OVER(PARTITION BY d.name ORDER BY e.salary DESC) AS rnk
+    RANK()OVER(PARTITION BY d.id ORDER BY e.salary DESC) AS rnk
     FROM Department d
     JOIN Employee e ON d.id=e.departmentId
 )
