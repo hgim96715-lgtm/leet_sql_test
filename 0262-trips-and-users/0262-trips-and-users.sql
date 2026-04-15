15c73defb7244619127152d8ed0f46ac1b7cbe73
hgim96715-lgtm <hgim96715@gmail.com>
2026-04-15 09:42:19 +0900

    Time: 592 ms (21.18%), Space: 0B (100%) - LeetHub

--- 0262-trips-and-users/0262-trips-and-users.sql
+++ 0262-trips-and-users/0262-trips-and-users.sql
@@ -11,7 +11,7 @@ WITH join_two AS (
 SELECT 
     request_at AS Day,
     ROUND(
-       SUM(status != 'completed') / COUNT(*),
+        SUM(IF(status != 'completed', 1, 0)) / COUNT(*), 
         2
     ) AS "Cancellation Rate"
 FROM join_two
