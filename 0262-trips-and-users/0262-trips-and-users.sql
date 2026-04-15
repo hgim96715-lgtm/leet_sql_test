ca10f56f33f5e4135c0ede116a6644d9d675ecf0
hgim96715-lgtm <hgim96715@gmail.com>
2026-04-15 09:32:37 +0900

    Time: 538 ms (52.31%), Space: 0B (100%) - LeetHub

--- 0262-trips-and-users/0262-trips-and-users.sql
+++ 0262-trips-and-users/0262-trips-and-users.sql
@@ -1,18 +1,8 @@
-WITH join_two AS (
-    SELECT 
-        t.status, 
-        t.request_at
-    FROM Trips t
-    JOIN Users u1 ON t.client_id = u1.users_id AND u1.banned = 'No'
-    JOIN Users u2 ON t.driver_id = u2.users_id AND u2.banned = 'No'
-    WHERE t.request_at BETWEEN '2013-10-01' AND '2013-10-03'
-)
-
 SELECT 
     request_at AS Day,
-    ROUND(
-        SUM(IF(status != 'completed', 1, 0)) / COUNT(*), 
-        2
-    ) AS "Cancellation Rate"
-FROM join_two
+    ROUND(AVG(status != 'completed'), 2) AS "Cancellation Rate"
+FROM Trips t
+JOIN Users u1 ON t.client_id = u1.users_id AND u1.banned = 'No'
+JOIN Users u2 ON t.driver_id = u2.users_id AND u2.banned = 'No'
+WHERE t.request_at BETWEEN '2013-10-01' AND '2013-10-03'
 GROUP BY request_at;
