374a4c017f600634648605630283e416e344e174
hgim96715-lgtm <hgim96715@gmail.com>
2026-06-18 05:25:01 +0900

    06-18-2026 1934-confirmation-rate

---
+++ PostgreSQL/Medium/1934-confirmation-rate/1934-confirmation-rate-06-18-2026-05-25-00.sql
@@ -0,0 +1,7 @@
+-- Write your PostgreSQL query statement below
+SELECT s.user_id,
+ROUND(SUM(CASE WHEN c.action='confirmed' THEN 1 ELSE 0 END)::Numeric/COUNT(s.user_id),2) AS confirmation_rate 
+FROM Signups s
+LEFT JOIN Confirmations c ON s.user_id=c.user_id
+GROUP BY s.user_id
+ORDER BY confirmation_rate 
